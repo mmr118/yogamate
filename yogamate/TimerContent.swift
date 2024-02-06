edebd04fadcb690cfceb3cc903864c29ba98165d
Monica Rondón <monirondon15@gmail.com>
2024-02-06 22:12:56 +0100

    Adds time values picker

--- yogamate/TimerContent.swift
+++ yogamate/TimerContent.swift
@@ -16,9 +16,40 @@ struct TimerContent: View {
 
     var body: some View {
 
-        Text("TIMER")
-        
+        VStack {
+
+            LabeledContent("HR", value: hourCount, format: .number)
+            LabeledContent("MIN", value: minuteCount, format: .number)
+            LabeledContent("SEC", value: secondCount, format: .number)
+
+            HStack {
+                valuePicker("hr", count: $hourCount, max: 24)
+                valuePicker("min", count: $minuteCount, max: 59)
+                valuePicker("sec", count: $secondCount, max: 59)
+
+            }
+
+        }
+        .padding()
+
     }
+
+    @ViewBuilder func valuePicker(_ title: String, count: Binding<Int>, max: Int) -> some View {
+        HStack {
+            Picker(title, selection: count) {
+                ForEach(0..<max) { value in
+                    Text(value, format: .number)
+                        .tag(value)
+                }
+            }
+            .pickerStyle(.wheel)
+            .labelsHidden()
+
+            Text(title)
+
+        }
+    }
+
 }
 
 
