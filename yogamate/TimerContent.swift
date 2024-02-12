7077fc4b2d9d688184055912db1bc771476ab725
Monica Rondón <monirondon15@gmail.com>
2024-02-12 20:44:07 +0100

    timer countdown working

--- yogamate/TimerContent.swift
+++ yogamate/TimerContent.swift
@@ -10,31 +10,39 @@ import SwiftUI
 
 struct TimerContent: View {
 
+    static let increment = 0.01
+
     @State var hourValue = 0
     @State var minuteValue = 0
-    @State var secondVslue = 0
+    @State var secondValue = 10
 
-    @State var 
+    @State var duration: Duration = .zero
+    @State var isTimerRunning = false
+    @State var timer = Timer.publish(every: Self.increment, on: .main, in: .common).autoconnect()
 
     var body: some View {
 
         VStack {
 
-            HStack {
-                valuePicker("hr", count: $hourValue, max: 24)
-                valuePicker("min", count: $minuteValue, max: 59)
-                valuePicker("sec", count: $secondVslue, max: 59)
-            }
+            if isTimerRunning {
+                countDownView()
+            } else {
 
-            LabeledContent("HR", value: hourValue, format: .number)
-            LabeledContent("MIN", value: minuteValue, format: .number)
-            LabeledContent("SEC", value: secondVslue, format: .number)
+                HStack {
+                    valuePicker("hr", count: $hourValue, max: 24)
+                    valuePicker("min", count: $minuteValue, max: 59)
+                    valuePicker("sec", count: $secondValue, max: 59)
+                }
+                .padding()
+                .border(.black, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+
+            }
 
             Button("Start") {
-
+                handleStart()
             }
             .buttonStyle(.borderedProminent)
-
+            .disabled(isTimerRunning == true)
 
         }
         .padding()
@@ -44,7 +52,7 @@ struct TimerContent: View {
     @ViewBuilder func valuePicker(_ title: String, count: Binding<Int>, max: Int) -> some View {
         HStack {
             Picker(title, selection: count) {
-                ForEach(0..<max) { value in
+                ForEach(Array(0...max), id: \.self) { value in
                     Text(value, format: .number)
                         .tag(value)
                 }
@@ -55,6 +63,27 @@ struct TimerContent: View {
             Text(title)
 
         }
+    }
+
+    @ViewBuilder private func countDownView() -> some View {
+        Text(duration.formatted(.time(pattern: .hourMinuteSecond(padHourToLength: 00, fractionalSecondsLength: 2))))
+            .onReceive(timer) { timer in
+                if duration > .zero {
+                    duration -= .seconds(Self.increment)
+                } else {
+                    isTimerRunning = false
+                }
+            }
+    }
+
+    private func handleStart() {
+        duration = .seconds((hourValue * 3600) + (minuteValue * 60) + secondValue)
+        timer = Timer.publish(every: Self.increment, on: .main, in: .common).autoconnect()
+        isTimerRunning = true
+    }
+
+    private func createDuration() {
+
     }
 
 }
